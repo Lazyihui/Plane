3fb585034af69d45849ee7f8fca9991e214278f8
包子 <chengbaozi6@qq.com>
2024-01-16 22:28:21 +0800

    begin

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+struct Ctx
+{
+    public int a;
+
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("hello world");
+        Raylib.InitWindow(800, 400, "plane");
+        while(!Raylib.WindowShouldClose()){
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.WHITE);
+            
+            Raylib.DrawCircle(200,200,20,Color.RED);
+
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+
+
+}
